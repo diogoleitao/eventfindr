de21c860417b69e2685d471cdd19e7aad81ae5ad
Diogo Leitão <diogo.n.leitao@gmail.com>
2018-01-23 22:54:32 +0000

    Copied files from other repo

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+    Navigator,
+    AppRegistry,
+    Text
+} from 'react-native';
+
+import OpentourAPP from './app/OpentourAPP';
+
+
+class Opentour extends React.Component {
+    render () {
+        //Transicao entre menus
+        return (
+            <OpentourAPP sceneTransition={Navigator.SceneConfigs.PushFromRight} />
+        );
+    }
+}
+
+AppRegistry.registerComponent('opentour2017', () => Opentour);
